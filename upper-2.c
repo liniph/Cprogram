bf8bd354d0321ab392b0a3f584f2ca6430477d2b
gokullp <gokullp2019@gmail.com>
2020-05-29 19:43:12 +0530

    changed z to 90

--- upper-2.c
+++ upper-2.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 // program to check whether the character is upper case or not
 int main(int argc, char *argv[]) {
-	
+
 	char ch;
 	printf("Enter one character");
 	scanf("%c",&ch);
-	if(ch>='A' && ch <='Z')
+	if(ch>='A' && ch <=90)
+	//A=65
 		printf("Upper case letter");
 	else
 		printf("Not Upper case");
 	return 0;
 }
-
